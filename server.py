8083a8183f746bacbaa56597b7f1f037c027cf52
Saul Baiman <saul.baizman@gmail.com>
2015-11-16 09:07:44 -0500

    Added working update button.

--- server.py
+++ server.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python
 # HueMenorah server
+#
+# To add syntax-color highlighting in PHPStorm, follow the general instructions below:
+# http://superuser.com/questions/843172/how-to-syntax-highlight-python-scripts-in-php-storm
