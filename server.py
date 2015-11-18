944495e7d0750c5057a593cf931d027b29638952
Saul Baizman <saul.baizman@gmail.com>
2015-11-17 22:24:11 -0500

    Added get_new_images function.

--- server.py
+++ server.py
@@ -21,6 +21,28 @@ What does this program do?
 
 '''
 
+def get_new_images ( recent_db_images, filesystem_images ) :
+
+    found_image_count = 0
+
+    new_images = []
+
+    while found_image_count < 9:
+
+        # choose a random image
+        one_image = choice ( filesystem_images )
+
+        # has it been seen recently?
+        if not one_image in recent_db_images:
+            # increment number of found images
+            found_image_count += 1
+            new_images.append ( one_image )
+        else:
+           continue
+
+    return new_images
+
+# TODO: TEST THIS LOOP.
 
 from sys import argv #used only for the basename call
 import os
@@ -40,8 +62,8 @@ from firebase import firebase
 from config import debug
 from config import imagery_directory
 from config import recent_imagery_db
+from config import recent_imagery_db_limit
 from config import fb_url
-
 
 # check command line arguments
 if len(argv) != 2:
@@ -59,22 +81,48 @@ mode = argv[1]
 # or have been picked within the last X time period
 # store the choices for future reference
 
-images = os.listdir ( imagery_directory )
+# open database of recently selected images
 
-one_image = choice ( images )
+recently_selected_imagery = shelve.open ( recent_imagery_db )
 
-# choose the screen mode
+# test the length of the content in the db
+if len ( recently_selected_imagery ) == 0 :
+    # seed the db, if needed, with a key and an empty list
+    recently_selected_imagery['images'] = []
+
+recent_db_images = recently_selected_imagery['images']
+
+filesystem_images = os.listdir ( imagery_directory )
+
+# TODO: choose the screen mode
+
+# find 9 new images that have not appeared recently
+
+new_unseen_images = get_new_images ( recent_db_images, filesystem_images )
 
 #####
 
-# update the database
-
-fb = firebase.FirebaseApplication( fb_url, None )
-
-# TODO: add other properties
+# TODO: add 'delay' and 'transition' properties
 update = { 'path' : one_image }
 
 # TODO: add authentication
 
-# TODO: add error checking here
+# update the database
+fb = firebase.FirebaseApplication( fb_url, None )
+
+# TODO: add error checking
 result = fb.patch('/0', update )
+
+# only track the most recent X images
+if len ( recent_db_images ) == recent_imagery_db_limit :
+    # remove oldest record
+    del ( recent_db_images[0] )
+
+# add image to recently selected images
+recent_db_images.append ( one_image )
+
+recently_selected_imagery['images'] = recent_db_images
+
+recently_selected_imagery.close ( )
+
+
