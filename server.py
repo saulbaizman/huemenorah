060726e093be52a118c315ce95313614b35d5c4c
Saul Baizman <saul.baizman@gmail.com>
2015-12-05 21:55:21 -0500

    Removed redundant code from server.py.

--- server.py
+++ server.py
@@ -21,36 +21,49 @@ What does this program do?
 
 '''
 
-def get_new_images ( recent_db_images, filesystem_images ) :
+# curl 'https://huemenorah.firebaseio.com/.json?download=myfilename.txt'
+# download JSON data
+
+################################################################################
+## Update the firebase database
+def update_firebase ( fb_url, screen, image ) :
+    # TODO: set 'delay' and 'transition' properties
+    update = { 'path' : image, 'delay' : '0', 'transition' : 'cross-dissolve' }
+
+    # TODO: add authentication
+    # TODO: add error checking
+    result = fb.patch('/' + str(screen), update )
+
+################################################################################
+## Get X random images
+def get_random_images ( image_count, recent_db_images, filesystem_images ) :
 
     found_image_count = 0
 
     new_images = []
 
-    mural_tiles = [ 'mural-tile-0.jpg', 'mural-tile-1.jpg', 'mural-tile-2.jpg', 'mural-tile-3.jpg', 'mural-tile-4.jpg', 'mural-tile-5.jpg', 'mural-tile-6.jpg', 'mural-tile-7.jpg', 'mural-tile-8.jpg' ]
-
-    instructions = 'instructions.jpg'
-
-    credits = 'credits.jpg'
-
-    while found_image_count < 9:
+    while found_image_count < image_count:
 
         # choose a random image
         one_image = choice ( filesystem_images )
 
         # TODO: add debugging info here
         # has it been seen recently? is it an image already in the hopper? is it a mural tile?
-        if not one_image in recent_db_images and not one_image in new_images and not one_image in mural_tiles:
+        if not one_image in recent_db_images and not one_image in new_images and not one_image in mural_tiles and one_image != credits_tile and one_image != instructions_tile:
             # increment number of found images
             found_image_count += 1
             new_images.append ( one_image )
-            print "added", one_image
+            if debug:
+                print "added", one_image
         else:
-            print "skipped", one_image
+            if debug:
+                print "skipped", one_image
             continue
 
     return new_images
 
+
+################################################################################
 
 from sys import argv #used only for the basename call
 import os
@@ -72,13 +85,18 @@ from config import imagery_directory
 from config import recent_imagery_db
 from config import recent_imagery_db_limit
 from config import fb_url
+from config import instructions_tile
+from config import credits_tile
+from config import mural_tiles
+
+################################################################################
 
 # check command line arguments
 if len(argv) != 2:
     print ""
     print "Incorrect number of arguments (" + str ( len(argv)-1 ) + "). Sample usage:"
     print ""
-    print os.path.basename ( argv[0] ) + " <mode>"
+    print os.path.basename ( argv[0] ), "<mode>"
     print ""
     exit()
 
@@ -103,40 +121,41 @@ recent_db_images = recently_selected_imagery['images']
 # get the list of files from the filesystem
 filesystem_images = os.listdir ( imagery_directory )
 
+# open connection to firebase
+fb = firebase.FirebaseApplication( fb_url, None )
+
 # TODO: choose the screen mode
 
 #mode = 'random' # for testing, assign the mode // comment this line out eventually
 
-# curl 'https://huemenorah.firebaseio.com/.json?download=myfilename.txt'
-# download JSON data
+################################################################################
 
 if mode == 'random':
+    if debug:
+        print "random mode"
 
     # find 9 new images that have not appeared recently
-
-    new_unseen_images = get_new_images ( recent_db_images, filesystem_images )
+    random_image_count = 9
+    new_unseen_images = get_random_images ( random_image_count, recent_db_images, filesystem_images )
 
     #####
-    updates = []
     for image in range ( len ( new_unseen_images ) ):
+
+        if debug:
+            print "processing", new_unseen_images[image], "..."
 
         # TODO: update all screens simultaneously, not one at a time.
 
-        # TODO: set 'delay' and 'transition' properties
-        update = { 'path' : new_unseen_images[image], 'delay' : '0', 'transition' : 'cross-dissolve' }
-        updates.append ( update )
-
-        # TODO: add authentication
-
         # update the database
-        fb = firebase.FirebaseApplication( fb_url, None )
-
-        # TODO: add error checking
-        result = fb.patch('/' + str(image), update )
+        if debug:
+            print "updating the firebase database: screen", str(image), "-", new_unseen_images[image]
+        update_firebase ( fb_url, str(image), new_unseen_images[image] )
 
         # only track the most recent X images
         if len ( recent_db_images ) == recent_imagery_db_limit :
             # remove oldest record
+            if debug:
+                print "deleting oldest image from recently used list:", recent_db_images[0]
             del ( recent_db_images[0] )
 
         # add image to recently selected images
@@ -144,28 +163,24 @@ if mode == 'random':
 
         recently_selected_imagery['images'] = recent_db_images
 
-
-#fb = firebase.FirebaseApplication( fb_url, None )
-#print updates
-#result = fb.put('/', updates )
-##result = fb.patch('/', updates )
-
+################################################################################
 
 if mode == 'mural':
-    print "mural mode"
-    # more code here
+    if debug:
+        print "mural mode"
+
     screen_count = 9
 
-    for image in range ( screen_count ) :
-        update = { 'path' : 'mural-tile-' + str(image) + '.jpg', 'delay' : '0', 'transition' : 'cross-dissolve' }
+    for image in range ( len ( mural_tiles ) ) :
+        if debug:
+            print "adding", image
+        if debug:
+            print "updating the firebase database: screen", str(image), "-", str(mural_tiles[image])
+        update_firebase ( fb_url, str(image), str(mural_tiles[image]) )
 
-        # TODO: turn this code into a function?
-        # update the database
-        fb = firebase.FirebaseApplication( fb_url, None )
+################################################################################
 
-        # TODO: add error checking
-        result = fb.patch('/' + str(image), update )
-
-
+if debug:
+    print recently_selected_imagery['images'], "(" + str ( len ( recently_selected_imagery['images'] ) ) + ")"
 recently_selected_imagery.close ( )
 
