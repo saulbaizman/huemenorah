808baec3b43ef48d759e76af722e2954dbd03923
Saul Baizman <saul.baizman@gmail.com>
2015-12-05 20:48:26 -0500

    Added credits and instructions variables to config.py.

--- config.py
+++ config.py
@@ -13,6 +13,12 @@ imagery_directory = 'images'
 
 recent_imagery_db = 'recent-imagery' # no ".db" filename extension
 
-recent_imagery_db_limit = 50
+recent_imagery_db_limit = 20
 
 fb_url = 'https://huemenorah.firebaseio.com/'
+
+credits_tile = 'credits.jpg'
+
+instructions_tile = 'instructions.jpg'
+
+mural_tiles = [ 'mural-0.jpg', 'mural-1.jpg', 'mural-2.jpg', 'mural-3.jpg', 'mural-4.jpg', 'mural-5.jpg', 'mural-6.jpg', 'mural-7.jpg', 'mural-8.jpg' ]
